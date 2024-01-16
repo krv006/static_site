62574fa6ae79e04a5b96ba69e888df3d20ea950a
Пользователь <rvkamronbek@gmail.com>
2024-01-17 00:35:57 +0500

    static_site

--- myapp/views.py
+++ myapp/views.py
@@ -2,20 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 def index (request):
+    
     return render(request , 'index.html' )
 
 def about (request):
+    
     return render(request , 'about.html' )
 
 
 def service (request):
+    
     return render(request , 'service.html' )
 
 
 def team (request):
+    
     return render(request , 'team.html' )
 
 
 
 def why (request):
+    
     return render(request , 'why.html' )
